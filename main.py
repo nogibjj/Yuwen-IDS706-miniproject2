ecafd1478c0d23db40aaf8036d4178b164ec43cb
Candice1121 <75481901+Candice1121@users.noreply.github.com>
2023-09-07 16:10:14 -0400

    fix assert value issues

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ def load_data(datapath):
 
 def get_data_descriptive_stats(dataframe):
     statistics = {
-        'Mean': dataframe.mean(),
-        'Median': dataframe.median(),
-        'StdDev': dataframe.std(),
-        'Min': dataframe.min(),
-        'Max': dataframe.max()
+        'Mean': dataframe.mean(numeric_only=True),
+        'Median': dataframe.median(numeric_only=True),
+        'StdDev': dataframe.std(numeric_only=True),
+        'Min': dataframe.min(numeric_only=True),
+        'Max': dataframe.max(numeric_only=True)
     }
     return pd.Series(statistics)
